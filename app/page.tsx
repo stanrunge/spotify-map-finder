ce701aba7085f5c3605f7df7fe209a666d1d546b
Stan Runge <stan@stanrunge.dev>
2023-12-25 01:59:44 +0100

    Add basic styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Track } from "@spotify/web-api-ts-sdk";
+import "./globals.css";
+
 import { getOsuMaps, getSpotifyMetadata } from "./actions";
 import { ChangeEvent, useState } from "react";
 import { Beatmapset } from "osu-web.js";
@@ -29,20 +30,36 @@ export default function Index() {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Search" onChange={handleInputChange} />
-        <button type="submit">Search</button>
-      </form>
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="relative px-4 py-10 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-10">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <input
+              type="text"
+              placeholder="Search"
+              onChange={handleInputChange}
+              className="block w-full p-3 rounded bg-gray-200 border border-transparent focus:outline-none"
+            />
+            <button
+              type="submit"
+              className="w-full py-3 px-4 text-white transition duration-300 ease-in-out bg-blue-600 hover:bg-blue-700 rounded"
+            >
+              Search
+            </button>
+          </form>
 
-      <h2>Results ({mapsets.length})</h2>
-      <ul>
-        {mapsets.map((mapset) => (
-          <li key={mapset.id}>
-            {mapset.artist} - {mapset.title}
-          </li>
-        ))}
-      </ul>
-    </>
+          <h2 className="pt-8 text-2xl font-semibold">
+            Results ({mapsets.length})
+          </h2>
+          <ul className="space-y-2">
+            {mapsets.map((mapset) => (
+              <li key={mapset.id} className="border p-4 rounded bg-gray-100">
+                {mapset.artist} - {mapset.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
